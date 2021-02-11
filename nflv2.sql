6ab9d0818432c4468b3070addf3d75d8cc535be7
grav85 <72578559+grav85@users.noreply.github.com>
2021-02-10 22:10:25 -0600

    Add files via upload

--- nflv2.sql
+++ nflv2.sql
@@ -3,41 +3,44 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- logical
+DROP TABLE IF EXISTS draft, combine;
+DROP TABLE IF EXISTS player;
 
-CREATE TABLE "player" (
-    "id" integer   NOT NULL,
-    "Player" varchar   NOT NULL,
-    "Pos" varchar   NOT NULL,
-    "School" varchar   NOT NULL,
-    CONSTRAINT "pk_player" PRIMARY KEY (
-        "id"
+CREATE TABLE player (
+    id serial,
+    player varchar NOT NULL,
+    school varchar,
+    CONSTRAINT pk_player PRIMARY KEY (
+        id
      )
 );
 
-CREATE TABLE "combine" (
-    "player_id" integer   NOT NULL,
-    "ht" varchar   NOT NULL,
-    "wt" Integer   NOT NULL,
-    "40yd" float   NOT NULL,
-    "vertical" Float   NOT NULL,
-    "bench" Integer   NOT NULL,
-    "broad_jump" integer   NOT NULL,
-    "3cone" float   NOT NULL,
-    "shuttle" float   NOT NULL,
-    "year" integer   NOT NULL
+
+CREATE TABLE combine (
+    player_id integer   NOT NULL,
+    "Ht" varchar,
+    "Wt" Integer,
+    "40yd" float,
+    "Vertical" Float,
+    "Bench" Integer,
+    "Broad Jump" integer,
+    "3Cone" float,
+    "Shuttle" float,
+    year integer
 );
 
-CREATE TABLE "draft" (
-    "player_id" integer   NOT NULL,
-    "rnd" integer   NOT NULL,
-    "pick_no" float   NOT NULL,
-    "conf" varchar   NOT NULL,
-    "year" integer   NOT NULL
+
+CREATE TABLE draft (
+    player_id integer   NOT NULL,
+    Rnd integer,
+    Pick_no float,
+    Conf varchar,
+    year integer
 );
 
-ALTER TABLE "combine" ADD CONSTRAINT "fk_combine_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("id");
+ALTER TABLE combine ADD CONSTRAINT fk_combine_player_id FOREIGN KEY(player_id)
+REFERENCES player (id);
 
-ALTER TABLE "draft" ADD CONSTRAINT "fk_draft_player_id" FOREIGN KEY("player_id")
-REFERENCES "player" ("id");
+ALTER TABLE draft ADD CONSTRAINT fk_draft_player_id FOREIGN KEY(player_id)
+REFERENCES player (id);
 
